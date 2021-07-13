4325371ada2585a0239909d3d33231cb58683e75
ehartjen <67296112+ehartjen@users.noreply.github.com>
2021-07-13 14:52:08 -0400

    Rewind Basic Build

--- js/site.js
+++ js/site.js
@@ -1,1 +1,32 @@
-//this is a comment
+//Step -1 get the word
+//step 2 reverse the word
+//strings are arrays of charachters 
+//we can use the same code to access the letters in a string
+//array[index] give me the value of array at position idex
+//string[index] - give me the letter in the string at position index 
+
+function getWord() {
+    let word = document.getElementById("word").Value;
+
+    let revWord = flipWord(word);
+
+    displayResult(word, revWord);
+};
+
+funtion flipWord(word) {
+
+    let revWord = "";
+
+    for (let index = word.length - 1; index = 0; index--) {
+        let letter = word[index];
+        revWord += letter;
+    }
+
+    return revWord;
+
+}
+
+function displayResults(word, revWord) {
+    let output = document.getElementById("output");
+    output.innerText = `you entered the word ${word}. Reversed is ${revWord}`;
+}
